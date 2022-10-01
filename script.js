87ad5c8278d97f65a8457482f76eada24d3ea22b
vipul Pandey <vipulpandey7860@gmail.com>
2022-10-02 00:16:48 +0530

    .

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
 function show() {
-    gsap.registerPlugin(ScrollTrigger);
-  
-  
-    const locoScroll = new LocomotiveScroll({
-      el: document.querySelector("#main"),
-      smooth: true
-    });
-    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-    locoScroll.on("scroll", ScrollTrigger.update);
-  
-    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-    ScrollTrigger.scrollerProxy("#main", {
-      scrollTop(value) {
-        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-      getBoundingClientRect() {
-        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-      },
-      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-      pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-    });
-  
-  
-  
-    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-  
-    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-    ScrollTrigger.refresh();
-  
-  }
+  gsap.registerPlugin(ScrollTrigger);
 
 
-function CursonAnimation(){
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-    document.addEventListener("mousemove", function (e) {
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+  });
 
-        var elem = document.createElement('div');
-        elem.setAttribute("class", "trail")
-    
-        elem.setAttribute("style", `left: ${e.clientX - 10}px; top:${e.clientY - 10}px;`);
-    
-        elem.onanimationend = () => {
-    
-    
-            elem.remove();
-        }
-    
-        document.body.insertAdjacentElement('beforeend', elem);
-    })
+
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+
 }
 
 
-function ProjectsAnimation(){
+function CursonAnimation() {
+
+  document.addEventListener("mousemove", function (e) {
+
+    var elem = document.createElement('div');
+    elem.setAttribute("class", "trail")
+
+    elem.setAttribute("style", `left: ${e.clientX - 10}px; top:${e.clientY - 10}px;`);
+
+    elem.onanimationend = () => {
+
+
+      elem.remove();
+    }
+
+    document.body.insertAdjacentElement('beforeend', elem);
+  })
+}
+
+
+function ProjectsAnimation() {
   gsap.to('.section', {
 
     scrollTrigger: {
-       trigger: '.section',
-       scroller: "#main",
-       start: "top 24%",
-       toggleActions: "play pause pause reverse",
+      trigger: '.section',
+      scroller: "#main",
+      start: "top 24%",
+      toggleActions: "play pause pause reverse",
       //  markers: true,
- 
+
     },
     scale: '1',
     top: '0%',
@@ -70,28 +70,28 @@ function ProjectsAnimation(){
     width: '100vw',
     height: '165vh',
     ease: ' Power2.easeOut',
-    backgroundImage :"none",
-    backgroundColor :"initial",
-    boxShadow:'none',
- 
- })
+    backgroundImage: "none",
+    backgroundColor: "initial",
+    boxShadow: 'none',
+
+  })
 
 
- gsap.to('.section h1', {
+  gsap.to('.section h1', {
 
-  scrollTrigger: {
-     trigger: '.section h1',
-     scroller: "#main",
-     start: "top 75%",
-     toggleActions: "play pause pause reverse",
-    //  markers: true,
+    scrollTrigger: {
+      trigger: '.section h1',
+      scroller: "#main",
+      start: "top 75%",
+      toggleActions: "play pause pause reverse",
+      //  markers: true,
 
-  },
-  display:'initial',
+    },
+    display: 'initial',
 
-})
+  })
 
-    
+
 }
 
 
@@ -103,40 +103,40 @@ function PageColorChange() {
 
 
   for (let i = 0; i < container.length; i++) {
-      container[i].addEventListener("mouseenter", function (dets) {
+    container[i].addEventListener("mouseenter", function (dets) {
 
-          const color = dets.path[0].dataset.color;
-          main.style.backgroundColor = color;
+      const color = dets.path[0].dataset.color;
+      main.style.backgroundColor = color;
 
-          circleTxt[i].style.display = 'initial';
-
-
-      });
-      container[i].addEventListener("mouseleave", function (dets) {
-
-          main.style.backgroundColor = "#15171b";
-          circleTxt[i].style.display = 'none';
+      circleTxt[i].style.display = 'initial';
 
 
+    });
+    container[i].addEventListener("mouseleave", function (dets) {
 
-      });
+      main.style.backgroundColor = "#15171b";
+      circleTxt[i].style.display = 'none';
+
+
+
+    });
 
   }
 
   // arrow and round text movement
 
   for (let i = 0; i < container.length; i++) {
-      container[i].addEventListener("mousemove", function (e) {
+    container[i].addEventListener("mousemove", function (e) {
 
 
-          var bndrectvals = container[i].getBoundingClientRect()
-          var xVal = Math.abs(e.clientX - bndrectvals.x);
-          var yVal = Math.abs(e.clientY - bndrectvals.y);
+      var bndrectvals = container[i].getBoundingClientRect()
+      var xVal = Math.abs(e.clientX - bndrectvals.x);
+      var yVal = Math.abs(e.clientY - bndrectvals.y);
 
-          circleTxt[i].style.left = xVal + "px";
-          circleTxt[i].style.top = yVal + "px";
+      circleTxt[i].style.left = xVal + "px";
+      circleTxt[i].style.top = yVal + "px";
 
-      })
+    })
 
 
   }
@@ -168,8 +168,90 @@ function TextAnimation() {
   }
 }
 
-show();
-CursonAnimation();
-ProjectsAnimation();
-PageColorChange();
-TextAnimation();
+function ProjectsMobileAnimation() {
+  gsap.to('.section', {
+
+    scrollTrigger: {
+      trigger: '.section',
+      scroller: "body",
+      start: "top 65%",
+      toggleActions: "play pause pause reverse",
+      //  markers: true,
+
+    },
+    scale: '1',
+    top: '0%',
+    borderTopRightRadius: '0',
+    borderTopLeftRadius: '0',
+    width: '100vw',
+    height: '165vh',
+    ease: ' Power2.easeOut',
+    backgroundImage: "none",
+    backgroundColor: "initial",
+    boxShadow: 'none',
+    left: '30%',
+
+  })
+
+
+  gsap.to('.section h1', {
+
+    scrollTrigger: {
+      trigger: '.section h1',
+      scroller: "body",
+      start: "top 75%",
+      toggleActions: "play pause pause reverse",
+      //  markers: true,
+
+    },
+    display: 'initial',
+
+  })
+
+
+}
+
+function TextMobileAnimation() {
+  var AnimateIt = document.querySelectorAll(".txts");
+
+  for (let i = 0; i < AnimateIt.length; i++) {
+
+    gsap.from(AnimateIt[i], {
+
+      scrollTrigger: {
+        trigger: AnimateIt[i],
+        scroller: "body",
+        start: "top 70%",
+        end: "top 50%",
+        scrub: .3,
+        // markers: true,
+      },
+      opacity: 0,
+      y: '40',
+      autoAlpha: 1,
+      stagger: .5,
+
+    })
+
+  }
+}
+
+
+if (window.innerWidth <= 500) {
+  show();
+
+  ProjectsMobileAnimation();
+  PageColorChange();
+  TextMobileAnimation();
+}
+
+else {
+  show();
+
+  CursonAnimation();
+  ProjectsAnimation();
+  PageColorChange();
+  TextAnimation();
+}
+
+
